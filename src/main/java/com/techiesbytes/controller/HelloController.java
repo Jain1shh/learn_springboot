6bd76428e8caa79b2d2a6ca462ef37d9edb2f210
Jain1shh <PrajapatiJainish@protonmail.com>
2025-07-05 14:23:01 +0530

    application.properties -> application.yml

--- src/main/java/com/techiesbytes/controller/HelloController.java
+++ src/main/java/com/techiesbytes/controller/HelloController.java
@@ -1,14 +1,18 @@
 package com.techiesbytes.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class HelloController {
 
+    @Value("${My.Name}")
+    private String Name;
+
     @RequestMapping(value = "/", method = RequestMethod.GET )
     @ResponseBody
     public String HelloWorld() {
-        return "Hello World";
+        return "Hello "+Name+"!";
     }
 }
